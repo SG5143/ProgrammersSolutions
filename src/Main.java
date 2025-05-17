a8851d5e2c83a106dff6298d6daec4aed7f9b483
SG5143 <sun5143a@gmail.com>
2025-05-17 22:05:31 +0900

    택배 상자 꺼내기

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,8 @@
 
 public class Main {
     public static void main(String[] args) {
-        Sol_389479 sol = new Sol_389479();
-        int[] arr = new int[]{0, 2, 3, 3, 1, 2, 0, 0, 0, 0, 4, 2, 0, 6, 0, 4, 2, 13, 3, 5, 10, 0, 1, 5};
-        int result = sol.solution(arr,3,5);
+        Sol_389478 sol = new Sol_389478();
+        int result = sol.solution(22, 6, 8);
 
         System.out.println(result);
     }
