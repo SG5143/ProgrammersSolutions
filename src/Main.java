9f08d1991a98680267492aceb2f5222a256bfbef
SG5143 <sun5143a@gmail.com>
2025-05-19 22:53:14 +0900

    유연근무제

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,12 @@
 
 public class Main {
     public static void main(String[] args) {
-        Sol_388353 sol = new Sol_388353();
+        Sol_388351 sol = new Sol_388351();
 
-        String[] storage = new String[]{"AZWQY", "CAABX", "BBDDA", "ACACA"};
-        String[] requests = new String[]{"A", "BB", "A"};
+        int[] schedules = new int[]{700, 800, 1100};
+        int[][] timelogs = new int[][]{{710, 2359, 1050, 700, 650, 631, 659}, {800, 801, 805, 800, 759, 810, 809}, {1105, 1001, 1002, 600, 1059, 1001, 1100}};
 
-        int result = sol.solution(storage, requests);
+        int result = sol.solution(schedules, timelogs,5);
 
         System.out.println(result);
     }
