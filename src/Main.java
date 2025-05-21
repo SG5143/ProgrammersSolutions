7a8f54b2aa29bdb08aabf0595eaf02b47ff09754
SG5143 <sun5143a@gmail.com>
2025-05-21 20:27:45 +0900

    완주하지 못한 선수 (퀵 정렬)

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,12 @@
 
 public class Main {
     public static void main(String[] args) {
-        Sol_340213 sol = new Sol_340213();
+        Sol_42576 sol = new Sol_42576();
 
-        String video_len = "10:55";
-        String pos = "00:05";
-        String op_start = "00:15";
-        String op_end = "06:55";
-        String[] commands = new String[]{"prev", "next", "next"};
+        String[] participant = new String[]{"leo", "eden", "kiki"};
+        String[] completion = new String[]{"eden", "kiki"};
 
-        String result = sol.solution(video_len, pos, op_start, op_end, commands);
+        String result = sol.solution(participant,completion);
 
         System.out.println(result);
     }
