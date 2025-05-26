3caeaa88539faf0f9df3145913497918479d3b36
SG5143 <sun5143a@gmail.com>
2025-05-26 18:46:46 +0900

    같은 숫자는 싫어 (스택/큐)

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,13 @@
 
 public class Main {
     public static void main(String[] args) {
-        Sol_43105 sol = new Sol_43105();
+        Sol_12906 sol = new Sol_12906();
 
-        int[][] triangle = new int[][]{{7}, {3, 8}, {8, 1, 0}, {2, 7, 4, 4}, {4, 5, 2, 6, 5}};
-        int result = sol.solution(triangle);
+        int[] arr = {1,1,3,3,0,1,1};
+        int[] result = sol.solution(arr);
 
-        System.out.println(result);
+        for(int i : result){
+            System.out.print(i+" ");
+        }
     }
 }
