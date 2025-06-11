5b231e47e7aa6db653b7fd1e1b7ef125655d32de
SG5143 <sun5143a@gmail.com>
2025-06-11 21:33:13 +0900

    [Lv2] H-Index (Sort) - Arrays.sort

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Sol_42746 sol = new Sol_42746();
+        Sol_42747 sol = new Sol_42747();
 
-        int[] number = new int[]{6, 10, 2};
+        int[] citations = new int[]{3, 0, 6, 1, 5};
 
-        System.out.println(sol.solution(number));
+        System.out.println(sol.solution(citations));
     }
 }
