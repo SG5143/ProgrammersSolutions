e84c807bb24366f24ae279c3b3bb3632c3d2021e
SG5143 <sun5143a@gmail.com>
2025-07-02 20:58:32 +0900

    [Lv1] 나머지가 1이 되는 수 찾기

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Sol_12934 sol = new Sol_12934();
-
-        long number =  121;
-        System.out.println(sol.solution(number));
+        Sol_87389 sol = new Sol_87389();
+        System.out.println(sol.solution(10));
     }
 }
